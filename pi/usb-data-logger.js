42bcac625ea4f766c85c0e14eb6d9bf488d4ab65
Todd Hibbs <todd@toddhibbs.com>
2021-09-03 18:11:33 -0700

    working version with bme280 logging

--- pi/usb-data-logger.js
+++ pi/usb-data-logger.js
@@ -80,7 +80,7 @@ function getArduinoLogFilename() {
     const num = parseInt(text, 10)
     return isNaN(num) ? null : num
   }).filter(n => n !== null).sort()
-  return `arduino-${sequences.length > 0 ? sequences[sequences.length -1] +1 : 0}.txt`
+  return `arduino-${sequences.length > 0 ? sequences[sequences.length - 1] + 1 : 0}.txt`
 }
 function getBME280LogFilename() {
   const files = fs.readdirSync('./bme280-logs')
