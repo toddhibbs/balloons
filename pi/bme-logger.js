cdab1374fbe81d36f54466c76f99d9e14400a056
Todd Hibbs <todd@tencentsolutions.com>
2022-10-28 20:24:49 -0700

    added standalone bme and camera scripts

---
+++ pi/bme-logger.js
@@ -0,0 +1,69 @@
+/*****************
+
+Periodically reads and logs data from the temp/pressure sensor
+
+******************/
+
+const fs = require('fs')
+const BME280 = require('bme280-sensor')
+const BME_LOGS_FOLDER = './bme280-logs'
+const BME_LOG_INTERVAL = 10000; // 10 seconds
+
+if (!fs.existsSync(BME_LOGS_FOLDER))
+{
+  fs.mkdirSync(BME_LOGS_FOLDER)
+}
+
+const bme280Filename = getBME280LogFilename()
+
+// BME280 sensor logging logic
+const options = {
+  i2cBusNo   : 1, // defaults to 1
+  i2cAddress : BME280.BME280_DEFAULT_I2C_ADDRESS() // defaults to 0x77
+}
+const bme280 = new BME280(options)
+// Read BME280 sensor data, repeat
+//
+const readSensorData = () => {
+  bme280.readSensorData()
+    .then((data) => {
+      // temperature_C, pressure_hPa, and humidity are returned by default.
+      // I'll also calculate some unit conversions for display purposes.
+      //
+      data.temperature_F = BME280.convertCelciusToFahrenheit(data.temperature_C);
+      data.pressure_inHg = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
+      data.timestamp = (new Date()).toJSON()
+
+      //console.log(`data = ${JSON.stringify(data, null, 2)}`);
+
+      fs.appendFileSync(`${BME_LOGS_FOLDER}/${bme280Filename}`, JSON.stringify(data, null, 2) + ',')
+
+      setTimeout(readSensorData, BME_LOG_INTERVAL);
+    })
+    .catch((err) => {
+      console.log(`BME280 read error: ${err}`);
+      setTimeout(readSensorData, BME_LOG_INTERVAL);
+    });
+}
+
+// Initialize the BME280 sensor
+//
+bme280.init()
+  .then(() => {
+    console.log('BME280 initialization succeeded');
+    readSensorData();
+  })
+  .catch((err) => console.error(`BME280 initialization failed: ${err} `))
+
+
+function getBME280LogFilename() {
+    const files = fs.readdirSync(BME_LOGS_FOLDER)
+    const sequences = files.map(f => {
+      const start = f.lastIndexOf('-') + 1
+      const end = f.indexOf('.')
+      const text = f.slice(start, end)
+      const num = parseInt(text, 10)
+      return isNaN(num) ? null : num
+    }).filter(n => n !== null).sort()
+    return `bme280-${sequences.length > 0 ? sequences[sequences.length - 1] + 1 : 0}.txt`
+  }
---
+++ pi/camera.js
@@ -0,0 +1,116 @@
+/*****************
+
+Periodically takes photos/videos and saves them
+
+******************/
+
+const fs = require('fs')
+const {StillCamera, StreamCamera, Codec} = require('pi-camera-connect')
+const VIDEOS_FOLDER = './videos'
+const PHOTOS_FOLDER = './photos'
+const PHOTO_INTERVAL = 6000; // 6 seconds when in photo intervalometer mode
+const VIDEO_DURATION = 1000 * 30; // 30 second videos
+const VIDEO_INTERVAL = 1000 * 60 * 5; // video every 5 minutes
+
+const INTERVALOMETER_COUNT = 50; // 
+
+// make sure folders exist
+if (!fs.existsSync(VIDEOS_FOLDER))
+{
+  fs.mkdirSync(VIDEOS_FOLDER)
+}
+if (!fs.existsSync(PHOTOS_FOLDER))
+{
+  fs.mkdirSync(PHOTOS_FOLDER)
+}
+
+let continueLoop = true; // give us a way to exit loop
+
+// loop indefinitely
+while (continueLoop) {
+    await TakePhotos()
+    await RecordVideo()
+}
+
+async function RecordVideo() {
+    await startVideo(getVideoFilename())
+    await new Promise(resolve => setTimeout(() => resolve(), VIDEO_DURATION));
+    await stopVideo()
+}
+
+async function TakePhotos() {
+    for(let i = 0; i < 30; i++) {
+        await takePhoto()
+    }
+}
+
+let streamCamera = new StreamCamera({
+    codec: Codec.H264
+})
+
+async function startVideo(filePath) {
+    try {
+        console.log('startVideo:', filePath)
+        const videoStream = streamCamera.createStream()
+        const writeStream = fs.createWriteStream(filePath)
+        videoStream.pipe(writeStream)
+        await streamCamera.startCapture()
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+async function stopVideo() {
+    try {
+        console.log('stopVideo')
+        await streamCamera.stopCapture()
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+async function takePhoto() {
+    try {
+        const stillCamera = new StillCamera()
+        let image = await stillCamera.takeImage()
+        fs.writeFileSync(`${PHOTOS_FOLDER}/${Date.now().toString()}.jpg`, image)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+let intervalometerActive = true;
+function startIntervalometer() {
+    if (intervalometerActive) {
+        console.log('startIntervalometer')
+        const stillCamera = new StillCamera()
+        stillCamera.takeImage().then(image => {
+        fs.writeFileSync(`${PHOTOS_FOLDER}/${Date.now().toString()}.jpg`, image)
+        setTimeout(startIntervalometer, PHOTO_INTERVAL)
+        })
+        .catch(err => console.log(err))
+    }
+}
+
+async function stopIntervalometer() {
+    console.log('stopIntervalometer')
+    intervalometerActive = false
+}
+  
+
+function getVideoFilename() {
+    const files = fs.readdirSync(VIDEOS_FOLDER)
+    const sequences = files.map(f => {
+        const start = f.lastIndexOf('-') + 1
+        const end = f.indexOf('.')
+        const text = f.slice(start, end)
+        const num = parseInt(text, 10)
+        return isNaN(num) ? null : num
+    }).filter(n => n !== null).sort()
+    return `video-${sequences.length > 0 ? sequences[sequences.length - 1] + 1 : 0}.h264`
+}
